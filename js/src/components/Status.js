34972338b82bf4e4fdb19fee82b090173fdb42a0
Inbound Now <david@inboundnow.com>
2015-04-30 18:29:59 -0700

    add update when time ago runs out on live page

--- js/src/components/Status.js
+++ js/src/components/Status.js
@@ -1,49 +1,74 @@
 var React = require('react'),
-SetIntervalMixin = require('./mixins/set-interval-mixin.js'),
 moment = require('moment');
-
+/*
+  TODO  update the component logic to show times or not shared with correct classes
+ */
 var Status = React.createClass({
+
   refreshClock: function() {
     var interval = 60000;
     this.interval = setInterval(this.forceUpdate.bind(this), interval);
   },
   clearTimer: function(){
+
       clearInterval(this.interval);
   },
   componentWillUnmount: function() {
       this.clearTimer();
   },
   componentDidMount: function() {
-      console.log('tets');
-      if(this.props.data) {
+      if(this.props.data && this.props.data.shared) {
         this.refreshClock();
       }
   },
+  triggerUpdate: function(){
+    var params = {
+      action: "delete",
+      row: this.props.rowData
+    };
+    jQuery('#stop-'+this.props.rowData.id).hide();
+    if (this.isMounted()) {
+      /* fix mounted issue. Invariant Violation */
+      setTimeout(function() {
+        jQuery(document).trigger('updateData', params);
+      }, 100);
+
+    }
+  },
   render: function(){
-    var spanClass = 'inactive';
-    if(this.props.data) {
+    var spanClass = 'inactive status-row';
+
+    if(this.props.data.time && this.props.data.shared) {
       var start = moment(),
-      expireTime   = moment(this.props.data),
+      expireTime = moment(this.props.data.time),
       timeDiff = expireTime.diff(start),
       humanReadableTime = expireTime.from(start),
       status;
-      //console.log(start);
+
+      var sharedText = (this.props.data.shared) ? <span className="shared-text">Shared</span> : "";
+      status = "- Expires " + humanReadableTime;
+      spanClass = 'active';
       //console.log(timeDiff);
-      // If timeDiff is
-      if (timeDiff > 0) {
-        status = "Expires " + humanReadableTime;
-        spanClass = 'active';
-      } else {
-        status = "Not Shared";
-        console.log('Clear the timer');
-        this.clearTimer();
+      if(timeDiff < 1) {
+         spanClass = 'inactive status-row';
+         status = "- Expired " + humanReadableTime;
+         this.triggerUpdate();
+         this.clearTimer();
+         /* todo fix invariation */
+
       }
 
     } else {
       status = "Not Shared";
+      console.log('Clear the timer');
+      this.clearTimer();
     }
 
-    return (<span className={spanClass}>{status}</span>);
+    return (
+          <span className={spanClass}>
+            {sharedText} {status}
+          </span>
+    );
 
   }
 });
